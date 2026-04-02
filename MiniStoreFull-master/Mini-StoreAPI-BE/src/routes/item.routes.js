c4ef6907008d62c4f11f125f96120df620f9c6f4
caupebautroi <minhduy7147@gmail.com>
2026-04-02 12:28:23 +0700

    Delete MiniStoreFull-master directory

--- MiniStoreFull-master/Mini-StoreAPI-BE/src/routes/item.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import * as itemController from "../controllers/item.controller"
-import { authorize, verifyToken } from "../middleware/auth.middleware";
-
-const router = express.Router();
-
-router.get("/", [verifyToken, authorize(["Admin", "Manager"])], itemController.getAll);
-
-export default router;
--- MiniStoreFull-master/MiniStoreClientReact-FE/src/api/item.api.js
+++
@@ -1,9 +0,0 @@
-import axiosClient from "./axiosClient"
-
-const itemApi = {
-    getAllItem: () => {
-        return axiosClient.get('/item');
-    }
-};
-
-export default itemApi;
